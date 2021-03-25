2f04180ed69a89c84bb978761a98a14b90082dd4
Rafał Bujok <rafal.bujok@gmail.com>
2021-03-25 23:16:11 +0100

    make calculator work like windows calc

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,56 +7,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  actions: string = '';
   currentNumber = '0';
-  operator = null;
-  firstOperand = null;
-  waitForSecondNumber = false;
+  operatorFirst: string = null;
+  operatorSecond: string = null;
+  firstOperand: string = null;
+  secondOperand: string = null;
+  numberComplated: boolean = false;
+  waitForSecondNumber: boolean = false;
 
-  public getNumber(num: string) {
+
+  public getNumber(num: string): void {
+    this.numberComplated = true;
     if (this.waitForSecondNumber) {
       this.currentNumber = num;
       this.waitForSecondNumber = false;
-    }
-    else {
-      if (this.currentNumber === '0') {
-        this.currentNumber = num;
-      } else this.currentNumber += num;
+    } else if (this.currentNumber === '0') {
+      this.currentNumber = num;
+    } else {
+      this.currentNumber += num;
     }
   }
-  public getDecimal() {
+  public getDecimal(): void {
     if (!this.currentNumber.includes('.')) {
       this.currentNumber += '.';
     }
   }
-  private doCalculation(op, secondOp) {
-    switch (op) {
-      case '+':
-        return this.firstOperand += secondOp;
-      case '-':
-        return this.firstOperand -= secondOp;
-      case '*':
-        return this.firstOperand *= secondOp;
-      case '/':
-        return this.firstOperand /= secondOp;
-      case '=':
-        return secondOp;
+
+  public getOperation(op: string): void {
+    if (!this.numberComplated && !this.firstOperand) {
+      this.firstOperand = '0';
+      this.actions = this.firstOperand;
     }
-  }
-  public getOperation(op: string) {
-    if (this.firstOperand === null) {
-      this.firstOperand = Number(this.currentNumber);
-    } else if (this.operator) {
-      const result = this.doCalculation(this.operator, Number(this.currentNumber));
-      this.currentNumber = String(result);
-      this.firstOperand = result;
+    if (this.numberComplated && !this.firstOperand) {
+      this.firstOperand = this.currentNumber;
+      this.actions = this.firstOperand;
+      this.numberComplated = false;
     }
-    this.operator = op;
+    if (this.numberComplated && !this.secondOperand) {
+      this.secondOperand = this.currentNumber;
+      this.actions = this.actions + this.secondOperand;
+      this.numberComplated = false;
+    }
+    if (this.firstOperand && !this.secondOperand) {
+      this.operatorFirst = op;
+      this.actions = this.firstOperand + this.operatorFirst;
+    }
+    if (this.firstOperand && this.secondOperand) {
+      this.operatorSecond = op;
+      this.actions = this.firstOperand + this.operatorFirst + this.secondOperand + this.operatorSecond;
+      this.currentNumber = this._doOperation(this.firstOperand, this.secondOperand, this.operatorFirst).toString();
+      if (this.operatorSecond !== '=') {
+        this.actions = this.currentNumber + op;
+        this.operatorFirst = this.operatorSecond;
+      } else {
+        this.operatorFirst = null;
+      }
+      this.firstOperand = this.currentNumber;
+      this.operatorSecond = null;
+      this.secondOperand = null;
+
+    }
+
     this.waitForSecondNumber = true;
+    console.log(this.firstOperand, this.secondOperand, this.operatorFirst, this.operatorSecond)
   }
-  public clear() {
+
+  private _doOperation(firstOperand: string, secondOperand: string, operator: string): number {
+    console.log(firstOperand, secondOperand, operator);
+    let result = 0;
+    switch (operator) {
+      case '+':
+        return result = (Number(firstOperand) + Number(secondOperand));
+      case '-':
+        return result = (Number(firstOperand) - Number(secondOperand));
+      case '*':
+        return result = (Number(firstOperand) * Number(secondOperand));
+      case '/':
+        return result = (Number(firstOperand) / Number(secondOperand));
+      case '=':
+        return result = (Number(secondOperand));
+    }
+  }
+
+  public clear(): void {
+    this.actions = '';
     this.currentNumber = '0';
+    this.operatorFirst = null;
+    this.operatorSecond = null;
     this.firstOperand = null;
-    this.operator = null;
+    this.secondOperand = null;
+    this.numberComplated = false;
     this.waitForSecondNumber = false;
   }
   constructor() { }
